4e7084d59bc9165475fbd987ba06875e9e39a1f3
erick-dsnk <erick.8bld@gmail.com>
2022-10-16 15:16:46 +0300

    added back welcome page and canvas container

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,26 @@
 import type { NextPage } from "next";
+import { useEffect, useState } from "react";
 
 import CanvasContainer from "../components/CanvasContainer";
+import WelcomePage from "../components/WelcomePage";
 
 const Home: NextPage = () => {
+  const [ isMobile, setIsMobile ] = useState(true);
+  const [ isWelcomePageVisible, setIsWelcomePageVisible ] = useState(true);
+
+  useEffect(() => {
+    setIsMobile(window.innerWidth <= 768);
+  }, []);
+
   return (
     <div className="home fade-in h-full flex flex-col items-center justify-center px-12 xl:px-0">
-      <CanvasContainer isMobile={true} />
+      {
+        (isWelcomePageVisible) ? (
+          <WelcomePage isVisible={isWelcomePageVisible} setIsVisible={setIsWelcomePageVisible} />
+        ) : (
+          <CanvasContainer isMobile={isMobile} />
+        )
+      }
     </div>
   );
 };
